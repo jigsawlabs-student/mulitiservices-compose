36a3b23123207df1ffecb6d32691aefaeb8552d8
Jeff Katzy <jeffreyerickatz@gmail.com>
2022-11-13 12:06:08 -0500

    add reading

---
+++ services/api/project/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+from postgres import Postgres
+import os
+
+app = Flask(__name__)
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+connection = Postgres(SQLALCHEMY_DATABASE_URI)
+
+connection.run("CREATE TABLE users (name text)")
+connection.run("INSERT INTO users VALUES ('bart simpson')")
+
+@app.route('/users')
+def index():
+    users = connection.all('SELECT * FROM users;')
+    return jsonify({'users': users})
+
+if __name__ == '__main__':
+    app.run(debug=True)
